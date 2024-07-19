da330310ba0a3632b1c1ff78f315dd14cc8e0a69
Anugrah Wardhani <ardhanwardhani@gmail.com>
2024-07-19 17:00:21 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EmployeeTable from "./EmployeeTable";
+
+function App(){
+  return(
+    <div>
+      <EmployeeTable/>
+    </div>
+  );
+}
+
+export default App;
